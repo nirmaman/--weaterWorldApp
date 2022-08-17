63b760123e142d88832f39e58c81c0c0d1e52f3e
nirmaman <nirmaman631@gmail.com>
2022-08-17 18:42:50 +0300

    prod environmment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+  XRapidAPIHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+  XRapidAPIKeyHeaderValue: '6eab09e49dmshe8566b779c7a4ccp14bbe3jsn0d45284cf13d'
 };
